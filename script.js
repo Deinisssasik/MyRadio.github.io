683876fdfa7a207fc0cc0a353a462c026abbae15
Deinisssasik <desis2006@mail.ru>
2025-05-08 13:32:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -41,14 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
             logo: "img/shanson.png"
         },
         {
-            id: "6",
-            name: "Радио Маяк",
-            url: "http://icecast.vgtrk.cdnvideo.ru/mayakfm_mp3_192kbps",
-            genre: "Разговорное",
-            region: "Москва",
-            logo: "img/Radio_Mayak.png"
-        },
-        {
             id: "7",
             name: "Вести FM",
             url: "http://icecast.vgtrk.cdnvideo.ru/vestifm",
@@ -120,6 +112,14 @@ document.addEventListener('DOMContentLoaded', function() {
             region: "Москва",
             logo: "img/nashe.jpg"
         },
+        {
+            id: "6",
+            name: "Радио Маяк",
+            url: "http://icecast.vgtrk.cdnvideo.ru/mayakfm_mp3_192kbps",
+            genre: "Разговорное",
+            region: "Москва",
+            logo: "img/Radio_Mayak.png"
+        }
     ];
 
     // Полный список из 60 станций доступен здесь (ссылка не рабочая):
